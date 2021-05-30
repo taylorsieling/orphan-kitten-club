f9d30748ba8b49b83505137fef2f50bf48924bf3
taylorsieling <taylorasieling@gmail.com>
2021-05-30 15:02:26 -0500

    setting up kitten controller

--- okc-backend/app/controllers/kitten_controller.rb
+++ okc-backend/app/controllers/kitten_controller.rb
@@ -1,15 +1,44 @@
 class KittensController < ApplicationController
+    before_action :set_kitten, only: [:show, :update, :destroy]
 
     def index
+        @kittens = Kitten.all 
+        render json: @kittens
+    end
+
+    def show
+        render json: @kitten
     end
     
     def create 
+        @kitten = Kitten.new(kitten_params)
+        if @kitten.save
+            render json: @kitten, status: :created, location: @kitten
+        else
+            render json: @kitten.errors, status: :unprocessable_entity
+        end
     end
 
     def update
+        if @kitten.update(kitten_params)
+            render json: @kitten
+          else
+            render json: @kitten.errors, status: :unprocessable_entity
+          end
     end
 
-    def delete
+    def destroy
+        @kitten.destroy
+    end
+
+    private 
+
+    def set_kitten
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def kitten_params 
+        params.require(:kitten).permit(:name, :sex, :age, :dob, :weight, :breed, :color, :pattern, :altered, :microchipped, :declawed, :intake_date, :intake_time, :location_found, :intake_type, :status, :current_location, :description, :portrait_url)
     end
 
 end
