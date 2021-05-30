9c272088ad679c9165a09b1ef0d2a9c0ea97384e
taylorsieling <taylorasieling@gmail.com>
2021-05-30 15:09:28 -0500

    fixing kittens renaming issues

--- okc-backend/config/routes.rb
+++ okc-backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :applications
-  resources :cats
+  resources :kittens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
