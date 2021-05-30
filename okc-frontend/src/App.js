aa83b763239a1e3f157ab63c4c639c6ee946dc08
taylorsieling <taylorasieling@gmail.com>
2021-05-30 18:53:52 -0500

    import BrowserRouter in App

--- okc-frontend/src/App.js
+++ okc-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
