16bbab5e486b335544de1e93b7be7783d4f1ebed
taylorsieling <taylorasieling@gmail.com>
2021-05-30 14:19:57 -0500

    setting up store

--- okc-frontend/src/index.js
+++ okc-frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'react-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'react-thunk'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
