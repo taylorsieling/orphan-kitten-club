df894ca095d773430618360461bc06d42b2c0979
taylorsieling <taylorasieling@gmail.com>
2021-05-30 21:07:13 -0500

    building router and home route

--- okc-frontend/src/App.js
+++ okc-frontend/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Home } from './containers/Home'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Orphan Kitten Club</h1>
-        <hr/>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Orphan Kitten Club</h1>
+          <hr/>
+
+          <Switch>
+            <Route exact path='/' component={ Home }>Home</Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
