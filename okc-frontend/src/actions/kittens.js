d563034df1e768be80e7a5049d37b49505cd6267
taylorsieling <taylorasieling@gmail.com>
2021-05-31 11:33:49 -0500

    updating kittens.js to class component

--- okc-frontend/src/actions/kittens.js
+++ okc-frontend/src/actions/kittens.js
@@ -1,5 +1,11 @@
 export const getKittens = () => {
     return (dispatch) => {
         dispatch({ type: "LOADING_KITTENS "})
+        fetch('/kittens')
+        .then(res => res.json())
+        .then(kittens => dispatch({
+            type: "KITTENS_LOADED", 
+            payload: kittens
+        }))
     }
 }
--- okc-frontend/src/containers/Kittens.js
+++ okc-frontend/src/containers/Kittens.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Kitten from '../components/Kitten'
+import { connect } from 'react-redux'
 
-export const Kittens = () => {
-    return (
-        <div>
-            <h3>Welcome! This is the Kitten Index Page!</h3>
-            <Kitten />
-        </div>
-    )
+class Kittens extends Component {
+
+    render() {
+        return (
+            <div>
+                <h3>Welcome! This is the Kitten Index Page!</h3>
+                <Kitten />
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    return {
+        ...state
+    }
+}
+
+export default connect(mapStateToProps)(Kittens)
