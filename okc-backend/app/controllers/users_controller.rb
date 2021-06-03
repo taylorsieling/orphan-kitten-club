ac408050595a0f649e97682be44012b1a8926bc0
taylorsieling <taylorasieling@gmail.com>
2021-06-03 11:23:28 -0500

    building users controller

--- okc-backend/app/controllers/users_controller.rb
+++ okc-backend/app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all 
+        render json: @users
+    end
+
     def show
         @user = User.find(params[:id])
         render json: @user
@@ -12,6 +17,12 @@ class UsersController < ApplicationController
         else
             render json: @user.errors, status: :unprocessable_entity
         end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 
 end
